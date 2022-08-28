f0835d42242b5fdc3aa4b385f4a39f3011fed870
Aniket Rai <prosniperaniket@gmail.com>
2022-08-28 11:57:35 +0530

    Initial commit

---
+++ clockIn.js
@@ -0,0 +1,44 @@
+const { Console } = require("console");
+const puppeteer = require("puppeteer");
+require("dotenv").config();
+puppeteer
+  .launch({ headless: process.env.HEADLESS == "true" ? true : false })
+  .then(async (browser) => {
+    const context = browser.defaultBrowserContext();
+    await context.overridePermissions(process.env.SUBDOMAINURI, [
+      "geolocation",
+    ]);
+    const page = await browser.newPage();
+    await page.setGeolocation({
+      latitude: Number(process.env.LATITUDE),
+      longitude: Number(process.env.LONGITUDE),
+    });
+
+    await page.goto(process.env.URI);
+    await page.waitForSelector("#email");
+    await page.type("#email", process.env.EMAIL);
+    await page.click(".login-from-btn");
+    await page.waitForSelector(".btn-keka-login");
+    await page.click(".btn-keka-login");
+    await page.waitForSelector("#password");
+    await new Promise((r) => setTimeout(r, 1000));
+    await page.type("#password", process.env.PASSWORD);
+    await page.click(".login-from-btn");
+
+    await page.waitForSelector(
+      "body > xhr-app-root > div > xhr-home > div > home-dashboard > div > div > div > div > div:nth-child(2) > div > div:nth-child(5) > div:nth-child(6) > home-attendance-clockin-widget > div > div.card-body.clear-padding.d-flex.flex-column.justify-content-between > div > div.h-100.d-flex.align-items-center > div > div.d-flex.align-items-center > div:nth-child(1) > button"
+    );
+    await page.click(
+      "body > xhr-app-root > div > xhr-home > div > home-dashboard > div > div > div > div > div:nth-child(2) > div > div:nth-child(5) > div:nth-child(6) > home-attendance-clockin-widget > div > div.card-body.clear-padding.d-flex.flex-column.justify-content-between > div > div.h-100.d-flex.align-items-center > div > div.d-flex.align-items-center > div:nth-child(1) > button"
+    );
+
+    await page.waitForSelector(
+      "body > xhr-app-root > div > xhr-home > div > home-dashboard > div > div > div > div > div:nth-child(2) > div > div:nth-child(5) > div:nth-child(6) > home-attendance-clockin-widget > div > div.card-body.clear-padding.d-flex.flex-column.justify-content-between > div > div.h-100.d-flex.align-items-center > div > div.d-flex.align-items-center > div:nth-child(1) > button"
+    );
+
+    await page.click(
+      "body > xhr-app-root > div > xhr-home > div > home-dashboard > div > div > div > div > div:nth-child(2) > div > div:nth-child(5) > div:nth-child(6) > home-attendance-clockin-widget > div > div.card-body.clear-padding.d-flex.flex-column.justify-content-between > div > div.h-100.d-flex.align-items-center > div > div.d-flex.align-items-center > div:nth-child(1) > button"
+    );
+    console.log("SUCCESSFULLY LOGGED IN");
+    await browser.close();
+  });
